e82db9fa2f2cdf0c0956f08df8a85724e90460aa
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-08-02 10:42:31 -0400

    ui fixes

--- components/SanityTypeCreator.tsx
+++ components/SanityTypeCreator.tsx
@@ -368,7 +368,7 @@ const Form = styled.form<{ isRoot?: boolean }>`
   gap: 0.15rem;
   padding: ${({ isRoot }) => (!isRoot ? "0" : " 0.5rem 2rem 1rem 2rem")};
   margin-top: ${({ isRoot }) => (!isRoot ? "0" : "25px")};
-  width: ${({ isRoot }) => (isRoot ? "clamp(300px, 80vw, 650px);" : "300px")};
+  width: ${({ isRoot }) => (isRoot ? "clamp(300px, 80vw, 720px);" : "300px")};
 `;
 
 const Horizontal = styled.div`
